6db2f0d53abfffe7e194ef84b9542712ab53cccf
sudarsangp <sudarsangp@gmail.com>
2014-11-05 09:44:46 +0800

    fixed error in minimum delay by converting to integer and implemented checking flights for cancelled attribute

--- scripts/home.js
+++ scripts/home.js
@@ -29,16 +29,7 @@ $(document).ready(function() {
 	$.get('/data/L_UNIQUE_CARRIERS.csv-', function(data){
 		var fromCsv = $.csv.toObjects(data);
 		carrierCsv = fromCsv;
-		// var selectElement = $("#airFly");
-
-		// for(var i=0; i<fromCsv.length;i++) {
-		// 	var codeValue = fromCsv[i].Code;
-		// 	var descriptionValue = fromCsv[i].Description;
-		// 	selectElement.append(new Option(descriptionValue, codeValue));
-		// };
-		// selectElement.html($("#airFly option").sort(function(a, b) {
-		// 	return a.text == b.text ? 0 : a.text < b.text ? -1 : 1
-		// }));
+		
 	})
 		.done(function() {
 			// alert('done ');
@@ -50,47 +41,16 @@ $(document).ready(function() {
 			// alert('cool');
 		});
 
-	// $.get('/data/L_AIRPORT.csv-', function(data){
-	// 	var fromCsv = $.csv.toObjects(data);
-	// 	// var selectElement = $("#destAir");
-
-	// 	// for(var i=0; i<fromCsv.length;i++) {
-	// 	// 	var codeValue = fromCsv[i].Code;
-	// 	// 	var descriptionValue = fromCsv[i].Description;
-	// 	// 	selectElement.append(new Option(descriptionValue, codeValue));
-	// 	// };
-	// 	// selectElement.html($("#destAir option").sort(function(a, b) {
-	// 	// 	return a.text == b.text ? 0 : a.text < b.text ? -1 : 1
-	// 	// }));
-	// })
-	// 	.done(function() {
-	// 		// alert('done ');
-	// 	})
-	// 	.fail(function() {
-	// 		alert(' error destination airport');
-	// 	})
-	// 	.always(function() {
-	// 		// alert('cool');
-	// 	});
-
 	var formatData = [];
 	var dataJson;
 	function iterateTestData(deptCode, arrCode, carrierCode) {
 		$.get('/data/ontime_data_test.json', function(data) {
-	//	console.log(data.length);
-	//	console.log(typeof data);
 		
 			dataJson = JSON.parse(data);
-			// console.log(dataJson[0].origin);
-			// console.log(dataJson[0].dest);
-			//console.log(deptCode);
+
 			var allCarrierDestinationCodes = getCarrierDestinationCodesForDeparture(dataJson, deptCode)
 			var deptCarrierCodes = allCarrierDestinationCodes.carrierCodes;
 			var destAirportCodes = allCarrierDestinationCodes.destAirCodes;
-			
-			// console.log(deptCarrierCodes);
-			// console.log(destAirportCodes);
-			
 
 			populateCarriers(deptCarrierCodes);
 			populateDestinationAirport(destAirportCodes);
@@ -100,6 +60,9 @@ $(document).ready(function() {
 		})
 		.done(function() {
 			renderGraph();
+		})
+		.fail(function() {
+			alert(' error getting json data from file');
 		});
 	};
 	
@@ -109,29 +72,19 @@ $(document).ready(function() {
 	function generateDataForGraph(dataJson, deptCode, arrCode, carrierCode, minDelay) {
 		var eachObject = {}
 			for(var i=0; i<dataJson.length; i++) {
-				if(dataJson[i].origin === deptCode && dataJson[i].dest === arrCode && dataJson[i].unique_carrier === carrierCode) {
-				//	console.log("match exists");
-					if(minDelay === (dataJson[i].dep_delay) + (dataJson[i].arr_delay)) {
+				if(dataJson[i].origin === deptCode && dataJson[i].dest === arrCode && dataJson[i].unique_carrier === carrierCode && dataJson[i].cancelled === "0") {
+
+					if(minDelay === parseInt(dataJson[i].dep_delay, 10) + parseInt(dataJson[i].arr_delay, 10)) {
 						eachObject["source"] = dataJson[i].origin;
 						eachObject["target"] = dataJson[i].dest;
 						eachObject["carrier"] = dataJson[i].unique_carrier;
-						eachObject["totaldelay"] = (dataJson[i].dep_delay) + (dataJson[i].arr_delay);
+						eachObject["totaldelay"] = parseInt(dataJson[i].dep_delay, 10) + parseInt(dataJson[i].arr_delay, 10);
+
 						formatData.push(eachObject);
 						eachObject = {};
 					}
 				}
-				// else if(dataJson[i].origin === deptCode) {
-				// 	eachObject["source"] = dataJson[i].origin;
-				// 	eachObject["target"] = dataJson[i].dest;
-				// 	formatData.push(eachObject);
-				// 	eachObject = {};
-				// }
-				// else if(dataJson[i].dest === arrCode) {
-				// 	eachObject["source"] = dataJson[i].origin;
-				// 	eachObject["target"] = dataJson[i].dest;
-				// 	formatData.push(eachObject);
-				// 	eachObject = {};
-				// }
+				
 			}
 	};
 
@@ -139,7 +92,7 @@ $(document).ready(function() {
 		var deptCarrierCodes = [];
 		var destAirportCodes = [];
 		for(var i=0; i<dataJson.length; i++) {
-				if(dataJson[i].origin === deptCode) {
+				if(dataJson[i].origin === deptCode && dataJson[i].cancelled === "0") {
 					deptCarrierCodes.push(dataJson[i].unique_carrier);
 					destAirportCodes.push(dataJson[i].dest);
 				}
@@ -154,8 +107,8 @@ $(document).ready(function() {
 		var min =  Number.MAX_SAFE_INTEGER;
 
 			for(var i=0; i<dataJson.length; i++) {
-				if(dataJson[i].origin === deptCode && dataJson[i].dest === arrCode && dataJson[i].unique_carrier === carrierCode) {
-					var totaldelay = (dataJson[i].dep_delay) + (dataJson[i].arr_delay);
+				if(dataJson[i].origin === deptCode && dataJson[i].dest === arrCode && dataJson[i].unique_carrier === carrierCode && dataJson[i].cancelled === "0") {
+					var totaldelay = parseInt(dataJson[i].dep_delay, 10) + parseInt(dataJson[i].arr_delay, 10);
 					if(min > totaldelay){
 						min = totaldelay;
 					}
@@ -186,20 +139,20 @@ $(document).ready(function() {
 	function populateCarriers(deptCarrierCodes) {
 		var selectElementCarrier = $("#airFly");
 		selectElementCarrier.find("option").remove();
-			var fromCsv = carrierCsv;
-			for(var i=0; i<fromCsv.length;i++) {
-				var codeValue = fromCsv[i].Code;
-				var descriptionValue = fromCsv[i].Description;
-				for(var j=0; j<deptCarrierCodes.length;j++){
-					if(deptCarrierCodes[j] === codeValue) {
-						selectElementCarrier.append(new Option(descriptionValue, codeValue));
-						break;
-					}
+		var fromCsv = carrierCsv;
+		for(var i=0; i<fromCsv.length;i++) {
+			var codeValue = fromCsv[i].Code;
+			var descriptionValue = fromCsv[i].Description;
+			for(var j=0; j<deptCarrierCodes.length;j++){
+				if(deptCarrierCodes[j] === codeValue) {
+					selectElementCarrier.append(new Option(descriptionValue, codeValue));
+					break;
 				}
-				
-			};
-			selectElementCarrier.html($("#airFly option").sort(function(a, b) {
-				return a.text == b.text ? 0 : a.text < b.text ? -1 : 1
+			}
+			
+		};
+		selectElementCarrier.html($("#airFly option").sort(function(a, b) {
+			return a.text == b.text ? 0 : a.text < b.text ? -1 : 1
 			}));
 	};
 
@@ -209,29 +162,20 @@ $(document).ready(function() {
 		
 		selectedOption = $(this).val();
 
-			var fromCsv = airportCsv;
-		//	console.log(fromCsv.length);
-			for(var i=0; i<fromCsv.length;i++) {
-				var codeValue = fromCsv[i].Code;
-				var descriptionValue = fromCsv[i].Description;
-				
-				if(selectedOption === codeValue) {
-					//console.log(descriptionValue);
-					
-					departureCode = codeValue;
-					// console.log(codeValue);
-					break;
-				}
-			};
-	
-			//console.log(departureCode);
-			var arrivalCode = $("#destAir").val();
-			var carrierCode = $("#airFly").val();
-			iterateTestData(departureCode, arrivalCode, carrierCode);
-	   // console.log(selectedOption);
-	   // console.log(selectedOption.length);
+		var fromCsv = airportCsv;
+		for(var i=0; i<fromCsv.length;i++) {
+			var codeValue = fromCsv[i].Code;
+			var descriptionValue = fromCsv[i].Description;
+			
+			if(selectedOption === codeValue) {
+				departureCode = codeValue;
+				break;
+			}
+		};
 
-
+		var arrivalCode = $("#destAir").val();
+		var carrierCode = $("#airFly").val();
+		iterateTestData(departureCode, arrivalCode, carrierCode);
 	});
 
 	$("#destAir").change(function() {
@@ -240,31 +184,22 @@ $(document).ready(function() {
 		
 		selectedOption = $(this).val();
 
-			var fromCsv = airportCsv;
-			//console.log(fromCsv.length);
-			for(var i=0; i<fromCsv.length;i++) {
-				var codeValue = fromCsv[i].Code;
-				var descriptionValue = fromCsv[i].Description;
-				
-				if(selectedOption === codeValue) {
-					//console.log(descriptionValue);
-					
-					arrivalCode = codeValue;
-					// console.log(codeValue);
-					break;
-				}
-			};
-			var departureCode = $("#depAir").val();
-			var carrierCode = $("#airFly").val();
+		var fromCsv = airportCsv;
+		for(var i=0; i<fromCsv.length;i++) {
+			var codeValue = fromCsv[i].Code;
+			var descriptionValue = fromCsv[i].Description;
+			
+			if(selectedOption === codeValue) {
+				arrivalCode = codeValue;
+				break;
+			}
+		};
+		var departureCode = $("#depAir").val();
+		var carrierCode = $("#airFly").val();
 
-			//iterateTestData(departureCode, arrivalCode, carrierCode);
-			var minDelay = getMinimumTotalDelay(dataJson, departureCode, arrivalCode, carrierCode);
-			generateDataForGraph(dataJson, departureCode, arrivalCode, carrierCode, minDelay);
-			renderGraph();
-	   // console.log(selectedOption);
-	   // console.log(selectedOption.length);
-
-
+		var minDelay = getMinimumTotalDelay(dataJson, departureCode, arrivalCode, carrierCode);
+		generateDataForGraph(dataJson, departureCode, arrivalCode, carrierCode, minDelay);
+		renderGraph();
 	});
 
 
@@ -274,173 +209,155 @@ $(document).ready(function() {
 		selectedOption = $(this).val();
 
 		var fromCsv = carrierCsv;
-			//console.log(fromCsv.length);
-			for(var i=0; i<fromCsv.length;i++) {
-				var codeValue = fromCsv[i].Code;
-				var descriptionValue = fromCsv[i].Description;
-				
-				if(selectedOption === codeValue) {
-					//console.log(descriptionValue);
-					
-					carrierCode = codeValue;
-					// console.log(codeValue);
-					break;
-				}
-			};
-			var departureCode = $("#depAir").val();
-			var arrivalCode = $("#destAir").val();
-			//iterateTestData(departureCode, arrivalCode, carrierCode);
-			var minDelay = getMinimumTotalDelay(dataJson, departureCode, arrivalCode, carrierCode);
-			generateDataForGraph(dataJson, departureCode, arrivalCode, carrierCode, minDelay);
-			renderGraph();
+		for(var i=0; i<fromCsv.length;i++) {
+			var codeValue = fromCsv[i].Code;
+			var descriptionValue = fromCsv[i].Description;
+			
+			if(selectedOption === codeValue) {
+				carrierCode = codeValue;
+				break;
+			}
+		};
+		var departureCode = $("#depAir").val();
+		var arrivalCode = $("#destAir").val();
 
+		var minDelay = getMinimumTotalDelay(dataJson, departureCode, arrivalCode, carrierCode);
+		generateDataForGraph(dataJson, departureCode, arrivalCode, carrierCode, minDelay);
+		renderGraph();
 	});
 
 	function renderGraph() {
+		$("svg").remove();
+		var links = formatData;
 
-			//d3.csv("data/test_small.csv", function(error, links) {
-			
-			// // var links = [{source: "Microsoft", target: "Amazon", type: "licensing"},
-   //           {source: "Microsoft", target: "Amazon", type: "suit"},
-   //           {source: "Samsung", target: "Apple", type: "suit"},
-   //           {source: "Microsoft", target: "Amazon", type: "resolved"}];
-   		//	console.log(window);
-   			$("svg").remove();
-   			var links = formatData;
-   		//	console.log(links.length);
-   		//	console.log(links[0]);
-		//	console.log(links[1]);
-			//console.log(links);
+		var color = d3.scale.category20();
+		var numberOfLinks = [];
+		for(var x=1; x<100;x++){
+			numberOfLinks.push(x);
+		}
+		
+		links.sort(function(a,b) {
+		    if (a.source > b.source) {return 1;}
+		    else if (a.source < b.source) {return -1;}
+		    else {
+		        if (a.target > b.target) {return 1;}
+		        if (a.target < b.target) {return -1;}
+		        else {return 0;}
+		    }
+		});
+		//any links with duplicate source and target get an incremented 'linknum'
+		for (var i=0; i<links.length; i++) {
+		    if (i != 0 &&
+		        links[i].source == links[i-1].source &&
+		        links[i].target == links[i-1].target) {
+		            links[i].linknum = links[i-1].linknum + 1;
+		        }
+		    else {links[i].linknum = 1;};
+		};
 
-			var color = d3.scale.category20();
-			var numberOfLinks = [];
-			for(var x=1; x<100;x++){
-				numberOfLinks.push(x);
-			}
-			
-			links.sort(function(a,b) {
-			    if (a.source > b.source) {return 1;}
-			    else if (a.source < b.source) {return -1;}
-			    else {
-			        if (a.target > b.target) {return 1;}
-			        if (a.target < b.target) {return -1;}
-			        else {return 0;}
-			    }
-			});
-			//any links with duplicate source and target get an incremented 'linknum'
-			for (var i=0; i<links.length; i++) {
-			    if (i != 0 &&
-			        links[i].source == links[i-1].source &&
-			        links[i].target == links[i-1].target) {
-			            links[i].linknum = links[i-1].linknum + 1;
-			        }
-			    else {links[i].linknum = 1;};
-			};
+		var nodes = {};
 
-			var nodes = {};
+		// Compute the distinct nodes from the links.
+		links.forEach(function(link) {
+		  link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
+		  link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
+		});
 
-			// Compute the distinct nodes from the links.
-			links.forEach(function(link) {
-			  link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
-			  link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
-			});
+		var w = 1000,
+		    h = 600;
 
-			var w = 1000,
-			    h = 600;
+		var force = d3.layout.force()
+		    .nodes(d3.values(nodes))
+		    .links(links)
+		    .size([w, h])
+		    .linkDistance(200)
+		    .charge(-150)
+		    .on("tick", tick)
+		    .start();
 
-			var force = d3.layout.force()
-			    .nodes(d3.values(nodes))
-			    .links(links)
-			    .size([w, h])
-			    .linkDistance(200)
-			    .charge(-150)
-			    .on("tick", tick)
-			    .start();
+		var svg = d3.select("#forceGraph").append("svg:svg")
+		    .attr("width", w)
+		    .attr("height", h);
 
-			var svg = d3.select("#forceGraph").append("svg:svg")
-			    .attr("width", w)
-			    .attr("height", h);
+		// Per-type markers, as they don't inherit styles.
+		svg.append("svg:defs").selectAll("marker")
+		    .data(numberOfLinks)
+		  .enter().append("svg:marker")
+		    .attr("id", String)
+		    .attr("viewBox", "0 -5 10 10")
+		    .attr("refX", 15)
+		    .attr("refY", -1.5)
+		    .attr("markerWidth", 6)
+		    .attr("markerHeight", 6)
+		    .attr("orient", "auto")
+		  .append("svg:path")
+		    .attr("d", "M0,-5L10,0L0,5");
 
-			// Per-type markers, as they don't inherit styles.
-			svg.append("svg:defs").selectAll("marker")
-			    .data(numberOfLinks)
-			  .enter().append("svg:marker")
-			    .attr("id", String)
-			    .attr("viewBox", "0 -5 10 10")
-			    .attr("refX", 15)
-			    .attr("refY", -1.5)
-			    .attr("markerWidth", 6)
-			    .attr("markerHeight", 6)
-			    .attr("orient", "auto")
-			  .append("svg:path")
-			    .attr("d", "M0,-5L10,0L0,5");
+		var path = svg.append("svg:g").selectAll("path")
+		    .data(force.links())
+		  .enter().append("svg:path")
+		    .attr("class", function(d) { return "link " + d.linknum; })
+		    .attr("id",function(d,i) { return "linkId_" + i; })
+		    .attr("marker-end", function(d) { return "url(#" + d.linknum + ")"; });
 
-			var path = svg.append("svg:g").selectAll("path")
-			    .data(force.links())
-			  .enter().append("svg:path")
-			    .attr("class", function(d) { return "link " + d.linknum; })
-			    .attr("id",function(d,i) { return "linkId_" + i; })
-			    .attr("marker-end", function(d) { return "url(#" + d.linknum + ")"; });
+		     var linktext = svg.append("svg:g").selectAll("g.linklabelholder").data(force.links());
+	
+		    linktext.enter().append("g").attr("class", "linklabelholder")
+		     .append("text")
+		     .attr("class", "linklabel")
+		    	 .style("font-size", "13px")
+		     .attr("x", "20")
+		    	 .attr("y", "-20")
+		     .attr("text-anchor", "start")
+		    	   .style("fill","#000")
+		    	 .append("textPath")
+		    .attr("xlink:href",function(d,i) { return "#linkId_" + i;})
+		     .text(function(d) { 
+		    	 return d.carrier; 
+		    	 });
+	 
 
-			     var linktext = svg.append("svg:g").selectAll("g.linklabelholder").data(force.links());
-    	
-			    linktext.enter().append("g").attr("class", "linklabelholder")
-			     .append("text")
-			     .attr("class", "linklabel")
-			    	 .style("font-size", "13px")
-			     .attr("x", "20")
-			    	 .attr("y", "-20")
-			     .attr("text-anchor", "start")
-			    	   .style("fill","#000")
-			    	 .append("textPath")
-			    .attr("xlink:href",function(d,i) { return "#linkId_" + i;})
-			     .text(function(d) { 
-			    	 return d.carrier; 
-			    	 });
-		 
+		var circle = svg.append("svg:g").selectAll("circle")
+		    .data(force.nodes())
+		  .enter().append("svg:circle")
+		    .attr("r", 5)
+		    .style("fill", function(d){ return color(d.linknum);})
+		    .call(force.drag);
 
-			var circle = svg.append("svg:g").selectAll("circle")
-			    .data(force.nodes())
-			  .enter().append("svg:circle")
-			    .attr("r", 5)
-			    .style("fill", function(d){ return color(d.linknum);})
-			    .call(force.drag);
+		var text = svg.append("svg:g").selectAll("g")
+		    .data(force.nodes())
+		  .enter().append("svg:g");
 
-			var text = svg.append("svg:g").selectAll("g")
-			    .data(force.nodes())
-			  .enter().append("svg:g");
+		// A copy of the text with a thick white stroke for legibility.
+		text.append("svg:text")
+		    .attr("x", 8)
+		    .attr("y", ".31em")
+		    .attr("class", "shadow")
+		    .text(function(d) { return d.name; });
 
-			// A copy of the text with a thick white stroke for legibility.
-			text.append("svg:text")
-			    .attr("x", 8)
-			    .attr("y", ".31em")
-			    .attr("class", "shadow")
-			    .text(function(d) { return d.name; });
+		text.append("svg:text")
+		    .attr("x", 8)
+		    .attr("y", ".31em")
+		    .text(function(d) { return d.name; });
 
-			text.append("svg:text")
-			    .attr("x", 8)
-			    .attr("y", ".31em")
-			    .text(function(d) { return d.name; });
+		// Use elliptical arc path segments to doubly-encode directionality.
+		function tick() {
+		  path.attr("d", function(d) {
+		    var dx = d.target.x - d.source.x,
+		        dy = d.target.y - d.source.y,
+		        dr = 150/d.linknum;  //linknum is defined above
+		    return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+		  });
 
-			// Use elliptical arc path segments to doubly-encode directionality.
-			function tick() {
-			  path.attr("d", function(d) {
-			    var dx = d.target.x - d.source.x,
-			        dy = d.target.y - d.source.y,
-			        dr = 150/d.linknum;  //linknum is defined above
-			    return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
-			  });
+		  circle.attr("transform", function(d) {
+		    return "translate(" + d.x + "," + d.y + ")";
+		  });
 
-			  circle.attr("transform", function(d) {
-			    return "translate(" + d.x + "," + d.y + ")";
-			  });
-
-			  text.attr("transform", function(d) {
-			    return "translate(" + d.x + "," + d.y + ")";
-			  });
-			};
+		  text.attr("transform", function(d) {
+		    return "translate(" + d.x + "," + d.y + ")";
+		  });
+		};
 		//});
-		//console.log(formatData.length);
 		formatData.length = 0;
 	};
 
